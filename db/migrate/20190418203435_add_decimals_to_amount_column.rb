d8f9787c2368fe927f9f340bd72e33f6e88758c4
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-05-07 18:28:01 -0400

    Update decimals in amount column

--- db/migrate/20190418203435_add_decimals_to_amount_column.rb
+++ db/migrate/20190418203435_add_decimals_to_amount_column.rb
@@ -1,5 +1,9 @@
 class AddDecimalsToAmountColumn < ActiveRecord::Migration[5.2]
-  def change
+  def up
+    change_column :expenses, :amount, :decimal, precision: 8, :scale => 2
+  end
+
+  def down
     change_column :expenses, :amount, :decimal, precision: 8, :scale => 2
   end
 end
