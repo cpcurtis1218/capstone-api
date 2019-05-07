109f1ee439acd96e8b200425f05a9cc3022855a8
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-05-07 16:29:13 -0400

    Updated schema with date column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_07_194953) do
+ActiveRecord::Schema.define(version: 2019_05_07_201419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_05_07_194953) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.string "date", null: false
     t.index ["user_id"], name: "index_expenses_on_user_id"
   end
 
